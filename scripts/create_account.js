469376c3b447e6976b81747a1ee2b9347b80c743
Dameon Wilson <thebrowncoder@gmail.com>
2019-11-28 23:51:11 -0500

    First Commit for CJFW Website

---
+++ scripts/create_account.js
@@ -0,0 +1,44 @@
+function create_account() {
+    ///////////////////// VARIABLE CREATION///////////////////////////
+    // create variables
+    'use strict';
+    var fname, lname, email, phone, ext, pword;
+    fname = document.getElementsByName('First Name').value;
+    lname = document.getElementsByName('Last Name').value;
+    email = document.getElementsByName('Email').value;
+    phone = document.getElementsByName('Phone').value;
+    ext = document.getElementsByName('Extension').value;
+    pword = document.getElementsByName('Password').value;
+    cpwd = document.getElementsByName('Confirm Password').value;
+
+    // store input of each textbox
+    console.log(fname + lname);
+    alert("Value inside the text boxes is: + fname + \" \" + lname");
+
+
+
+
+
+
+
+    /////////////////// REQUIRED VARIABLES://////////////////////////////////////////////////////////////////////////////////////////// 
+
+    // first name
+    // last name
+    // email address
+    // phone number
+    // password, and confirm password
+
+    //////////////////// VALIDATION TESTS://////////////////////////////////////////////////////////////////////////////////////////////
+
+    // validate name textboxes formatting (letters & no integers or special characters)
+
+    // validate email textbox formatting (text@domain's name.[extension])
+
+    // validate phone number textbox formatting (###-###-####)
+
+    // validate extension textbox formatting (####)
+    //     then append a comma(,) to the beginning of the extension variable
+
+    // USE PASSWORD SERVICE TO 
+};
---
+++ scripts/header_switcher.js
@@ -0,0 +1,2 @@
+var h = console.log("hello");
+prompt("The way we speak here in America is " + h + ".");
